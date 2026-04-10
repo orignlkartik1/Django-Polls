e5147cb39c313f77343e347444af57cca05e4804
orignlkartik1 <kart964364@gmail.com>
2026-04-10 17:11:16 +0530

    changes in pages

--- Django-Polls/polls/views.py
+++ Django-Polls/polls/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse as ht
 def index(request):
     return ht("Hello World")
 # Create your views here.
+
+def detail(request,question_id):
+    return ht("You are looking at the question",question_id)
+
+def results(request,question_id):
+    return ht("You are looking at the results",question_id)
+
+def vote(request,question_id):
+    return ht("You are looking at the results",question_id)

4a8a2deba9d1b6ea6a1eb9fbec9025dd95e2b412
orignlkartik1 <kart964364@gmail.com>
2026-04-10 17:23:34 +0530

    new changes

--- Django-Polls/polls/views.py
+++ Django-Polls/polls/views.py
@@ -6,10 +6,10 @@ def index(request):
 # Create your views here.
 
 def detail(request,question_id):
-    return ht("You are looking at the question",question_id)
+    return ht("You are looking at the question %s" % question_id)
 
 def results(request,question_id):
-    return ht("You are looking at the results",question_id)
+    return ht("You are looking at the results %s" %question_id)
 
 def vote(request,question_id):
-    return ht("You are looking at the results",question_id)
+    return ht("You are looking at the results %s" %question_id)

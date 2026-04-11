261c1edcdfc17f2827448541a42a473d608cf35c
orignlkartik1 <kart964364@gmail.com>
2026-04-11 22:00:17 +0530

    new changes

--- Django-Polls/polls/views.py
+++ Django-Polls/polls/views.py
@@ -1,11 +1,65 @@
 from django.shortcuts import render
 from django.http import HttpResponse as ht, Http404
 from django.template import loader
-from .models import Question
+from .models import Question,Vote
 
+from django.db.models import F
+from django.http import HttpResponseRedirect
+
+from django.urls import reverse
+from django.views import generic
 
 def index(request):
     latest_question_list = Question.objects.order_by('-put_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
-# Create your views here.
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+# ...
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice.get(pk=request.POST["choice"])
+    except (KeyError, Vote.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+from django.shortcuts import get_object_or_404, render
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
